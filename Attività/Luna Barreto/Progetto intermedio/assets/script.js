f72ff4ab3406ebe76d3ce7034502f06aa58aa1ad
LunaBarreto <barretoluna.b@gmail.com>
2024-09-14 11:22:54 +0200

    ultime modifiche

--- Attività/Luna Barreto/Progetto intermedio/assets/script.js
+++ Attività/Luna Barreto/Progetto intermedio/assets/script.js
@@ -1,38 +1,15 @@
 const titolo = document.getElementById("titolo");
-const ora = document.getElementById("ora")
-// const data = document.getElementById("data")
-
-let macDate = new Date();
-const giorniSettimana = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"];
+const data = document.getElementById("data");
+const macDate = new Date();
+const giorniSettimana = ["Lunedì","Martedì","Mercoledì", "Giovedì","Venerdì","Sabato", "Domenica"];
 const numGiorno = macDate.getDay();
 
+
 if(numGiorno >= 7){
-    titolo.innerHTML = "Ciao! Buona " + giorniSettimana[numGiorno-1];
+    titolo.innerHTML= "Buona " + giorniSettimana[numGiorno-1];
 } else {
-    titolo.innerHTML = "Ciao! Buon " + giorniSettimana[numGiorno-1];
+    titolo.innerHTML= "Buon " + giorniSettimana[numGiorno-1];
 }
 
+data.innerHTML = macDate.getDate()+ "/" + (macDate.getMonth()+1) + "/" + macDate.getFullYear();
 
-
-aggiornamentoOra();
-
-setInterval(aggiornamentoOra, 100);
-
-function aggiornamentoOra(){
-    macDate = new Date();
-    aggiungiZero();
-    ora.innerHTML = "sono le " + aggiungiZero(macDate.getHours()) + ":" + aggiungiZero(macDate.getMinutes()) + ":" + aggiungiZero(macDate.getSeconds());
-    // data.innerHTML = macDate.getDate() + "/" + (macDate.getMonth()+1) + "/" + macDate.getFullYear();
-}
-
-function aggiungiZero(valore){
-    let orario = 0;
-    if(valore<=9){
-        orario = "0" + valore
-    } else {
-        orario = valore
-    };
-
-    return orario;
-
-}
